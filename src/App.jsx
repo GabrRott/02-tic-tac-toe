389bf4d11c18b1ea7490cd3ee1a25b16ba9e5875
GabrRott <gabrielroatta@gmail.com>
2024-03-14 12:08:33 -0300

    Adding turns functions

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,13 @@ const TURNS = {
 }
 
 
-const Square = ({children, updateBoard, index}) => {
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  const handleClick = () =>{
+    updateBoard(index)
+  }
   return(
-    <div className='square'>
+    <div on Click={updateBoard} className='square'>
       {children}
     </div>
   )
@@ -18,7 +22,13 @@ const Square = ({children, updateBoard, index}) => {
 
 function App() {
 const [board, setBoard] = useState(Array(9).fill(null))
-const [turn, setTurn] = useState(TURNS, X) 
+const [turn, setTurn] = useState(TURNS. X) 
+const updateBoard = (index) =>{
+  const newBoard = [...board]
+  newBoard[index] = turn
+  const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+  setTurn(newTurn)
+}
 
   return (
       <main className='board'>    
@@ -30,12 +40,22 @@ const [turn, setTurn] = useState(TURNS, X)
                      <Square
                      key={index}
                      index={index}
+                     updateBoard={updateBoard}
                      >
-                      {index}
+                      {board[index]}
                      </Square>
                     )
                   })
                 }
+
+              </section>
+              <section className='turn'>
+                <Square isSelected={turn === TURNS.X}>
+                  {TURNS.X}
+                </Square>
+                <Square isSelected={turn === TURNS.O}>
+                  {TURNS.O}
+                </Square>
 
               </section>
       </main>
